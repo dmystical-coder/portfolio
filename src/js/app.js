92a19a3212bf52014f2723e2ec2ea4688b1fab9c
dmystical-coder <dmystical.coder@gmail.com>
2024-11-04 18:10:49 +0100

    style: Update favicon links and add close icon; enhance navbar functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,19 @@
 // navbar
 
 const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
 
 const navigationLinks = document.querySelector(".navigation-links");
+close.addEventListener('click', () => {
+    console.log('close');
+    hamburger.style.display = 'block';
+    close.style.display = 'none';
+    navigationLinks.classList.add('hidden');
+})
 
 hamburger.addEventListener('click', () => {
-    navigationLinks.classList.toggle('hidden');
+    close.style.display = 'block';
+    close.style.zIndex = '1000';
+    hamburger.style.display = 'none';
+    navigationLinks.classList.remove('hidden');
 })
