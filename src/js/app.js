c26afa4f4f94eabfcebce4d3259132faf79124a4
dmystical-coder <dmystical.coder@gmail.com>
2024-11-04 20:23:03 +0100

    style: Adjust navbar padding and update menu toggle behavior

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,19 @@
 
 const showMenu = document.getElementById("show-menu");
 const hideMenu = document.getElementById("hide-menu");
-
+const header = document.querySelector(".header");
 const navigationLinks = document.querySelector(".navigation-links");
 
 showMenu.addEventListener("click", () => {
+    header.style.paddingBottom = "5rem";
     navigationLinks.classList.add("active");
     showMenu.style.display = "none";
     hideMenu.style.display = "block";
-
 });
 
 hideMenu.addEventListener("click", () => {
     navigationLinks.classList.remove("active");
+    header.style.paddingBottom = "2.5rem";
     hideMenu.style.display = "none";
     showMenu.style.display = "block";
 });
